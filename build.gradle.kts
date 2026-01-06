b19d614dc035e685267cae565fbb61871f8a10e7
Termux User <termux@pasiflonet.com>
2026-01-06 16:49:06 +0200

    Fix: Combine Local Libs with Aliyun Mirror and 4GB RAM

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
+// Root build.gradle
 buildscript {
     repositories {
-        google()
-        mavenCentral()
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        gradlePluginPortal()
     }
     dependencies {
-        // שינמוך קל לגרסה סופר-יציבה של הפלאגין
+        // שימוש בגרסה יציבה מאוד
         classpath("com.android.tools.build:gradle:8.1.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
@@ -12,8 +14,11 @@ buildscript {
 
 allprojects {
     repositories {
+        // קודם כל Aliyun (מהיר), אחר כך המקומיים, ובסוף המקוריים לגיבוי
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://jitpack.io") }
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,9 @@
+pluginManagement {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        gradlePluginPortal()
+    }
+}
 rootProject.name = "PasiflonetMobile"
 include(":app")
