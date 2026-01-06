fd7c11c8044947b894898b085c1bdeb275aa4001
Termux User <termux@pasiflonet.com>
2026-01-06 16:22:21 +0200

    Fix: Add JCenter backup repo and force refresh dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.0")
@@ -14,6 +15,8 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        // שרת גיבוי למקרה ש-Maven Central קורס או חוסם (פותר שגיאות 403)
+        maven { url = uri("https://jcenter.bintray.com/") }
         maven { url = uri("https://jitpack.io") }
     }
 }
